c75d9c552c9fbfc1c67cb270d5670b0d043b5e5e
EvonByway <evon@byway.travel>
2024-06-15 19:12:07 +0100

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,29 @@
+import { CreatePost } from "~/app/_components/create-post";
+import Link from "next/link";
+import { api } from "~/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      Hello
+    </main>
+  );
+}
+
+async function CrudShowcase() {
+  const latestPost = await api.post.getLatest();
+
+  return (
+    <div className="w-full max-w-xs">
+      {latestPost ? (
+        <p className="truncate">Your most recent post: {latestPost.name}</p>
+      ) : (
+        <p>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
+  );
+}
