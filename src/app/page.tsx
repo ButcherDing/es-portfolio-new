a04cc2243855a72432d52ea42f784b3cdf5e17cb
ButcherDing <evon.scott@gmail.com>
2024-07-08 15:15:54 +0100

    added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
+import { TopNav } from "./_components/topNav";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic"; // might not be necessary
 
 export default async function Home() {
-  const posts = await db.query.posts.findMany();
-  console.log("posts", posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => asc(model.id),
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello</h1>
-      <h2>Posts</h2>
-      {posts.map((post) => (
-        <div>{post.name}</div>
+      <TopNav />
+      <h2>Images</h2>
+      {images.map((image, index) => (
+        <div key={image.id + "" + index}>
+          <img src={image.url} alt={image.name} />
+          {image.name}
+        </div>
       ))}
     </main>
   );
 }
-
-// some change
